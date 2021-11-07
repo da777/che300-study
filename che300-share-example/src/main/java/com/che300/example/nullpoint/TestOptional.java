aca6d36d421bddd844e952e21246f3171b57182d
liujialiang <1308656780@qq.com>
2021-11-08 00:03:56 +0800

    <feat> add String-JVM example

--- che300-share-example/src/main/java/com/che300/example/nullpoint/TestOptional.java
+++ che300-share-example/src/main/java/com/che300/example/nullpoint/TestOptional.java
@@ -13,23 +13,26 @@ public class TestOptional {
 
     public static void main(String[] args) {
         UserInfo userInfo = new UserInfo();
+
+    }
+
+    public static void start(UserInfo userInfo){
         //错误示例
         String result = userInfo.getAddress().getCity().getZipCode().getZipCode();
-        
-//        第一种方法
-        if (userInfo != null) {
-            Address address = userInfo.getAddress();
-            if (address != null) {
-                City city = address.getCity();
-                if (city != null) {
-                    ZipCode zipCode1 = city.getZipCode();
-                    if (zipCode1 != null) {
-                        System.out.println(zipCode1.getZipCode());
-                    }
+        System.out.println(result);
+
+        //第一种方法
+        Address address = userInfo.getAddress();
+        if (address != null) {
+            City city = address.getCity();
+            if (city != null) {
+                ZipCode zipCode1 = city.getZipCode();
+                if (zipCode1 != null) {
+                    System.out.println(zipCode1.getZipCode());
                 }
             }
         }
-        
+
         //第二种方法
 
         String s = Optional.ofNullable(userInfo)
@@ -41,4 +44,5 @@ public class TestOptional {
                 .orElse(null);
         System.out.println(s);
     }
+
 }
